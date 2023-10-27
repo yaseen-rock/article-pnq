26cc8858a229a30389cc602d23f9d12f12a069b4
Usman <memerboyemiology444@gmail.com>
2023-10-27 13:04:48 +0500

    first commit

---
+++ typescript-version/src/@core/layouts/components/shared-components/footer/FooterContent.tsx
@@ -0,0 +1,54 @@
+// ** Next Import
+import Link from 'next/link'
+
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import { Theme } from '@mui/material/styles'
+import { styled } from '@mui/material/styles'
+import Typography from '@mui/material/Typography'
+import useMediaQuery from '@mui/material/useMediaQuery'
+
+const StyledCompanyName = styled(Link)(({ theme }) => ({
+  fontWeight: 500,
+  textDecoration: 'none',
+  color: `${theme.palette.primary.main} !important`
+}))
+
+const LinkStyled = styled(Link)(({ theme }) => ({
+  textDecoration: 'none',
+  color: `${theme.palette.text.secondary} !important`,
+  '&:hover': {
+    color: `${theme.palette.primary.main} !important`
+  }
+}))
+
+const FooterContent = () => {
+  // ** Var
+  const hidden = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'))
+
+  return (
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
+      <Typography sx={{ mr: 2, display: 'flex', color: 'text.secondary' }}>
+        {`© ${new Date().getFullYear()}, Made with `}
+        <Box component='span' sx={{ mx: 1, color: 'error.main' }}>
+          ❤️
+        </Box>
+        
+      </Typography>
+      {hidden ? null : (
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', '& :not(:last-child)': { mr: 4 } }}>
+          <Typography
+            target='_blank'
+            component={LinkStyled}
+            href='https://demos.pixinvent.com/vuexy-nextjs-admin-template/documentation'
+          >
+            Documentation
+          </Typography>
+         
+        </Box>
+      )}
+    </Box>
+  )
+}
+
+export default FooterContent
