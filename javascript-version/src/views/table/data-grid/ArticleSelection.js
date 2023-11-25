1ef1015c4604210b35e40aa80865d9eae5dd1097
yaseen-rock <yaseen.ragib@gmail.com>
2023-11-25 23:35:57 +0530

    Add new feature

--- javascript-version/src/views/table/data-grid/ArticleSelection.js
+++ javascript-version/src/views/table/data-grid/ArticleSelection.js
@@ -14,17 +14,19 @@ import Stack from '@mui/material/Stack'
 import Divider from '@mui/material/Divider'
 import TextField from '@mui/material/TextField'
 import { DataGrid } from '@mui/x-data-grid'
+import ClearIcon from '@mui/icons-material/Clear'
+import Button from '@mui/material/Button' // Import Button
+import Checkbox from '@mui/material/Checkbox'
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
+import CheckBoxIcon from '@mui/icons-material/CheckBox'
 
-// ** React DatePicker
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
+// ** MUI-X DatePicker
 
-// ** Custom Components
-import CustomChip from 'src/@core/components/mui/chip'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
 
-// ** Utils Import
-import { getInitials } from 'src/@core/utils/get-initials'
-
+// ** MUI icons
 import SearchIcon from '@mui/icons-material/Search'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EmailIcon from '@mui/icons-material/Email'
@@ -37,7 +39,7 @@ import SvgIcon from '@mui/material/SvgIcon'
 // 1D Icon
 const OneDIcon = props => (
   <SvgIcon {...props}>
-    <text x='50%' y='50%' fill='grey' fontSize='14px' text-anchor='middle' alignment-baseline='middle'>
+    <text x='50%' y='50%' fill='#7367F0' fontSize='14px' text-anchor='middle' alignment-baseline='middle'>
       1D
     </text>
   </SvgIcon>
@@ -46,8 +48,8 @@ const OneDIcon = props => (
 // 2D Icon
 const TwoDIcon = props => (
   <SvgIcon {...props}>
-    <text x='50%' y='50%' fill='grey' fontSize='14px' text-anchor='middle' alignment-baseline='middle'>
-      2D
+    <text x='50%' y='50%' fill='#7367F0' fontSize='14px' text-anchor='middle' alignment-baseline='middle'>
+      7D
     </text>
   </SvgIcon>
 )
@@ -55,8 +57,8 @@ const TwoDIcon = props => (
 // 3D Icon
 const ThreeDIcon = props => (
   <SvgIcon {...props}>
-    <text x='50%' y='50%' fill='grey' fontSize='14px' text-anchor='middle' alignment-baseline='middle'>
-      3D
+    <text x='50%' y='50%' fill=' #7367F0' fontSize='14px' text-anchor='middle' alignment-baseline='middle'>
+      1M
     </text>
   </SvgIcon>
 )
@@ -216,6 +218,104 @@ const articles = [
     description:
       'Proident sunt in culpa qui officia deserunt mollit anim id est laborum. Nisi ut aliquip ex ea commodo consequat.',
     date: '2024-08-16'
+  },
+  {
+    id: 21,
+    article: 'Sample Article 21',
+    shortHeading: 'Incididunt Ut Labore',
+    description: 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    date: '2024-09-01'
+  },
+  {
+    id: 22,
+    article: 'Sample Article 22',
+    shortHeading: 'Lorem Ipsum Dolor',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    date: '2024-09-15'
+  },
+  {
+    id: 23,
+    article: 'Sample Article 23',
+    shortHeading: 'Reprehenderit In Voluptate',
+    description:
+      'Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.',
+    date: '2024-10-02'
+  },
+  {
+    id: 24,
+    article: 'Sample Article 24',
+    shortHeading: 'Cupidatat Non Proident',
+    description:
+      'Cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nisi ut aliquip ex ea commodo consequat.',
+    date: '2024-10-18'
+  },
+  {
+    id: 25,
+    article: 'Sample Article 25',
+    shortHeading: 'Duis Aute Irure Dolor',
+    description:
+      'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    date: '2024-11-05'
+  },
+  {
+    id: 26,
+    article: 'Sample Article 26',
+    shortHeading: 'Tempor Incididunt Ut Labore',
+    description:
+      'Tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    date: '2024-11-21'
+  },
+  {
+    id: 27,
+    article: 'Sample Article 27',
+    shortHeading: 'Consectetur Adipiscing Elit',
+    description: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    date: '2024-12-08'
+  },
+  {
+    id: 28,
+    article: 'Sample Article 28',
+    shortHeading: 'Ut Enim Ad Minim Veniam',
+    description:
+      'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    date: '2024-12-25'
+  },
+  {
+    id: 29,
+    article: 'Sample Article 29',
+    shortHeading: 'Nisi Ut Aliquip Ex Ea Commodo',
+    description:
+      'Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    date: '2025-01-10'
+  },
+  {
+    id: 30,
+    article: 'Sample Article 30',
+    shortHeading: 'Dolor Sit Amet',
+    description: 'Dolor sit amet, consectetur adipiscing elit. Nisi ut aliquip ex ea commodo consequat.',
+    date: '2025-01-26'
+  },
+  {
+    id: 31,
+    article: 'Sample Article 31',
+    shortHeading: 'Dolor Sit Amet',
+    description: 'Dolor sit amet, consectetur adipiscing elit. Nisi ut aliquip ex ea commodo consequat.',
+    date: '2023-11-23'
+  },
+  {
+    id: 32,
+    article: 'Sample Article 32',
+    shortHeading: 'Dolor Sit Amet',
+    description: 'Dolor sit amet, consectetur adipiscing elit. Nisi ut aliquip ex ea commodo consequat.',
+    date: '2023-10-28'
+  },
+  {
+    id: 33,
+    article: 'Sample Article 33',
+    shortHeading: 'Dolor Sit Amet',
+    description: 'Dolor sit amet, consectetur adipiscing elit. Nisi ut aliquip ex ea commodo consequat.',
+    date: '2023-11-25'
   }
 
   // Add more articles as needed
@@ -250,9 +350,9 @@ const columns = [
     minWidth: 40,
     headerName: 'Select',
     field: 'select',
+
     renderCell: params => (
-      <input
-        type='checkbox'
+      <Checkbox
         checked={params.row.isSelected}
         onChange={() => params.api.selectRow(params.row.id, !params.row.isSelected, false)}
       />
@@ -286,19 +386,55 @@ const TableSelection = () => {
   const [selectedStartDate, setSelectedStartDate] = useState(null)
   const [selectedEndDate, setSelectedEndDate] = useState(null)
   const [filterPopoverAnchor, setFilterPopoverAnchor] = useState(null)
+  const [searchQuery, setSearchQuery] = useState('')
+  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false)
+  const [selectedDuration, setSelectedDuration] = useState(null)
 
-  // Filter articles based on the selected date range
+  // Filter articles based on the selected date range and search query
   const filteredArticles = useMemo(() => {
-    if (!selectedStartDate || !selectedEndDate) {
-      return articles
+    let result = articles
+
+    // Apply date range filter
+    if (selectedStartDate && selectedEndDate) {
+      result = result.filter(article => {
+        const articleDate = new Date(article.date)
+
+        return articleDate >= selectedStartDate && articleDate <= selectedEndDate
+      })
     }
 
-    return articles.filter(article => {
-      const articleDate = new Date(article.date)
+    // Apply search query filter
+    if (searchQuery) {
+      result = result.filter(
+        article =>
+          article.article.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          article.shortHeading.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          article.description.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    }
 
-      return articleDate >= selectedStartDate && articleDate <= selectedEndDate
-    })
-  }, [selectedStartDate, selectedEndDate])
+    // Apply duration filter
+    if (selectedDuration) {
+      const currentDate = new Date()
+      const startDate = new Date(currentDate)
+
+      if (selectedDuration === 1) {
+        startDate.setDate(currentDate.getDate() - 1)
+      } else if (selectedDuration === 7) {
+        startDate.setDate(currentDate.getDate() - selectedDuration)
+      } else if (selectedDuration === 30) {
+        startDate.setMonth(currentDate.getMonth() - 1)
+      }
+
+      result = result.filter(article => {
+        const articleDate = new Date(article.date)
+
+        return articleDate >= startDate && articleDate <= currentDate
+      })
+    }
+
+    return result
+  }, [selectedStartDate, selectedEndDate, searchQuery, selectedDuration])
 
   // Divide articles into left and right columns
   const leftArticles = filteredArticles.filter((_, index) => index % 2 === 0)
@@ -312,6 +448,11 @@ const TableSelection = () => {
   // Close the date filter popover
   const closeFilterPopover = () => {
     setFilterPopoverAnchor(null)
+  }
+
+  // Function to toggle search bar visibility
+  const toggleSearchBarVisibility = () => {
+    setIsSearchBarVisible(prev => !prev)
   }
 
   const handleDelete = () => {
@@ -346,18 +487,15 @@ const TableSelection = () => {
   }
 
   const handleFilter1D = () => {
-    // Implement 1D filtering logic
-    console.log('Filtering 1D')
+    setSelectedDuration(1)
   }
 
-  const handleFilter2D = () => {
-    // Implement 2D filtering logic
-    console.log('Filtering 2D')
+  const handleFilter7D = () => {
+    setSelectedDuration(7)
   }
 
-  const handleFilter3D = () => {
-    // Implement 3D filtering logic
-    console.log('Filtering 3D')
+  const handleFilter1M = () => {
+    setSelectedDuration(30)
   }
 
   return (
@@ -368,35 +506,45 @@ const TableSelection = () => {
         <Typography variant='h6' sx={{ flex: '1' }}>
           Article List
         </Typography>
-        <IconButton onClick={handleSearch} sx={{ mr: 1 }}>
+        {/* Search Bar */}
+        {isSearchBarVisible && (
+          <TextField
+            label='Search Articles'
+            variant='outlined'
+            size='small'
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+          />
+        )}
+        <IconButton onClick={toggleSearchBarVisibility} sx={{ color: '#7367F0', mr: 1 }}>
           <SearchIcon />
         </IconButton>
-        <IconButton onClick={handleDelete}>
+        <IconButton onClick={handleDelete} sx={{ color: '#7367F0' }}>
           <DeleteIcon />
         </IconButton>
-        <IconButton onClick={handleEmail}>
+        <IconButton onClick={handleEmail} sx={{ color: '#7367F0' }}>
           <EmailIcon />
         </IconButton>
-        <IconButton onClick={handleImage}>
+        <IconButton onClick={handleImage} sx={{ color: '#7367F0' }}>
           <ImageIcon />
         </IconButton>
-        <IconButton onClick={handleDownload}>
+        <IconButton onClick={handleDownload} sx={{ color: '#7367F0' }}>
           <DownloadIcon />
         </IconButton>
-        <IconButton onClick={handleRssFeed}>
+        <IconButton onClick={handleRssFeed} sx={{ color: '#7367F0' }}>
           <RssFeedIcon />
         </IconButton>
-        <IconButton onClick={openFilterPopover}>
+        <IconButton onClick={openFilterPopover} sx={{ color: '#7367F0' }}>
           <DateRangeIcon />
         </IconButton>
         {/* Custom Icons for Filtering */}
         <IconButton onClick={handleFilter1D}>
           <OneDIcon />
         </IconButton>
-        <IconButton onClick={handleFilter2D}>
+        <IconButton onClick={handleFilter7D}>
           <TwoDIcon />
         </IconButton>
-        <IconButton onClick={handleFilter3D}>
+        <IconButton onClick={handleFilter1M}>
           <ThreeDIcon />
         </IconButton>
       </Toolbar>
@@ -410,27 +558,37 @@ const TableSelection = () => {
           horizontal: 'right'
         }}
       >
-        <Stack spacing={2} p={2} sx={{ minWidth: '200px', minHeight: '400px' }}>
+        <Stack spacing={2} p={2} sx={{ minWidth: '200px', minHeight: '200px' }}>
           <Typography variant='subtitle2'>Filter by Date Range</Typography>
           <Divider />
-          {/* Use react-datepicker instead of MUI DatePicker */}
-          <DatePicker
-            selected={selectedStartDate}
-            onChange={date => setSelectedStartDate(date)}
-            selectsStart
-            startDate={selectedStartDate}
-            endDate={selectedEndDate}
-            customInput={<TextField variant='standard' />}
-          />
-          <DatePicker
-            selected={selectedEndDate}
-            onChange={date => setSelectedEndDate(date)}
-            selectsEnd
-            startDate={selectedStartDate}
-            endDate={selectedEndDate}
-            minDate={selectedStartDate}
-            customInput={<TextField variant='standard' />}
-          />
+          {/* Use mui-x DatePicker */}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Box display='flex' alignItems='center'>
+              <DatePicker label='Start Date' value={selectedStartDate} onChange={date => setSelectedStartDate(date)} />
+              {selectedStartDate && (
+                <IconButton onClick={() => setSelectedStartDate(null)}>
+                  <ClearIcon />
+                </IconButton>
+              )}
+            </Box>
+            <Box display='flex' alignItems='center'>
+              <DatePicker label='End Date' value={selectedEndDate} onChange={date => setSelectedEndDate(date)} />
+              {selectedEndDate && (
+                <IconButton onClick={() => setSelectedEndDate(null)}>
+                  <ClearIcon />
+                </IconButton>
+              )}
+            </Box>
+          </LocalizationProvider>
+          {/* Clear both dates at the same time */}
+          <Button
+            onClick={() => {
+              setSelectedStartDate(null)
+              setSelectedEndDate(null)
+            }}
+          >
+            Clear
+          </Button>
         </Stack>
       </Popover>
       {/* DataGrid */}
