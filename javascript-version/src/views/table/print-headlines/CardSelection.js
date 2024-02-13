394cbba0dca76a026b3d54161f3c0164afe9b8c6
yaseen-rock <yaseen.ragib@gmail.com>
2024-02-13 15:32:21 +0530

    implemented card selection

--- javascript-version/src/views/table/print-headlines/CardSelection.js
+++ javascript-version/src/views/table/print-headlines/CardSelection.js
@@ -1,26 +1,113 @@
-// ** React Import
-import React, { useState } from 'react'
-
-// ** MUI Imports
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Toolbar from '@mui/material/Toolbar'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import Typography from '@mui/material/Typography'
 import CardHeader from '@mui/material/CardHeader'
-import TrendingUpIcon from '@mui/icons-material/TrendingUp' // Import TrendingUp icon
+import TrendingUpIcon from '@mui/icons-material/TrendingUp'
 import Grid from '@mui/material/Grid'
+import CircularProgress from '@mui/material/CircularProgress'
+import Table from '@mui/material/Table'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableContainer from '@mui/material/TableContainer'
+import TableRow from '@mui/material/TableRow'
 
-const cardsData = [
-  { title: 'Avendas Capital', description: 'Description for Avendas Capital goes here.' },
-  { title: 'Infosys', description: 'Description for Infosys goes here.' },
-  { title: 'ITC BSL Group', description: 'Description for ITC BSL Group goes here.' },
-  { title: 'IKEA', description: 'Description for IKEA goes here.' }
-]
+// ** Redux
+import { useSelector } from 'react-redux'
+import { selectSelectedClient } from 'src/store/apps/user/userSlice'
 
 const CardSelection = () => {
-  // ** State
   const [isContainerOpen, setContainerOpen] = useState(false)
+  const [companies, setCompanies] = useState([])
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(true)
+  const selectedClient = useSelector(selectSelectedClient)
+  const clientId = selectedClient ? selectedClient.clientId : null
+
+  const fetchArticlesForCompanies = async companyIds => {
+    try {
+      const storedToken = localStorage.getItem('accessToken')
+
+      const articlesPromises = companyIds.map(companyId =>
+        axios
+          .get('http://51.68.220.77:8001/latestArticlesForClientCompany/', {
+            headers: {
+              Authorization: `Bearer ${storedToken}`
+            },
+            params: {
+              clientId: clientId,
+              companyId: companyId
+            }
+          })
+          .then(response => {
+            console.log(`Received response for company ${companyId}:`, response?.data?.articles || [])
+
+            return response
+          })
+          .catch(error => {
+            console.error(`Error fetching articles for company ${companyId}:`, error)
+
+            return null
+          })
+      )
+
+      console.log('Sending requests for news articles for all companies...')
+
+      articlesPromises.forEach((promise, index) => {
+        const companyId = companyIds[index]
+        console.log(`Sending request for company ${companyId}...`)
+      })
+
+      const articlesResponses = await Promise.all(articlesPromises)
+
+      console.log('Received responses for news articles for all companies:', articlesResponses)
+
+      const allArticles = articlesResponses.flatMap(response => response?.data?.articles || [])
+      setArticles(allArticles.slice(0, 5))
+
+      return articlesResponses
+    } catch (error) {
+      console.error('Error fetching articles:', error)
+
+      return null
+    }
+  }
+
+  useEffect(() => {
+    const fetchUserDataAndCompanies = async () => {
+      try {
+        setLoading(true)
+        const storedToken = localStorage.getItem('accessToken')
+        if (storedToken && clientId) {
+          const response = await axios.get('http://51.68.220.77:8001/companyListByClient/', {
+            headers: {
+              Authorization: `Bearer ${storedToken}`
+            },
+            params: {
+              clientId: clientId
+            }
+          })
+          setCompanies(response.data.companies)
+
+          const companyIds = response.data.companies.map(company => company.companyId)
+          const articlesResponses = await fetchArticlesForCompanies(companyIds)
+          setLoading(false)
+        } else {
+          setCompanies([])
+          setArticles([])
+          setLoading(false)
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error)
+        setLoading(false)
+      }
+    }
+
+    fetchUserDataAndCompanies()
+  }, [clientId])
 
   const handleOpenContainer = () => {
     setContainerOpen(true)
@@ -33,26 +120,60 @@ const CardSelection = () => {
   return (
     <Card>
       <Toolbar>
-        <Button
-          startIcon={<TrendingUpIcon />} // Icon placed inside the button
-          onClick={handleOpenContainer}
-        >
+        <Button startIcon={<TrendingUpIcon />} onClick={handleOpenContainer}>
           Latest Competitive News
         </Button>
       </Toolbar>
 
       {isContainerOpen && (
         <>
-          <Grid container spacing={2} justifyContent='center' p={4}>
-            {cardsData.map((card, index) => (
-              <Grid item xs={12} sm={6} md={3} key={index}>
-                <Card sx={{ width: '100%', height: 270, textAlign: 'center' }}>
-                  <CardHeader title={card.title} />
-                  <Typography variant='body1'>{card.description}</Typography>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
+          {loading ? (
+            <Box display='flex' justifyContent='center' alignItems='center' height={300}>
+              <CircularProgress />
+            </Box>
+          ) : (
+            <Grid container spacing={2} justifyContent='center' p={4}>
+              {companies.map((company, index) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                  <Card sx={{ width: '100%', height: '100%', textAlign: 'center' }}>
+                    <CardHeader title={company.companyName} />
+                    <Typography variant='body1'>{company.description}</Typography>
+                    {articles
+                      .filter(article => article.companies.some(comp => comp.id === company.companyId))
+                      .map((article, articleIndex) => (
+                        <TableContainer key={articleIndex}>
+                          <Table>
+                            <TableBody>
+                              <TableRow>
+                                <TableCell>
+                                  <div>
+                                    <Typography variant='h6'>{article.headline}</Typography>
+                                  </div>
+                                  <div>
+                                    <Typography variant='body2'>
+                                      {`${article.publication} - (${new Date(article.articleDate).toLocaleDateString(
+                                        'en-US',
+                                        {
+                                          month: 'short',
+                                          day: 'numeric',
+                                          year: 'numeric'
+                                        }
+                                      )})`}
+                                    </Typography>
+                                  </div>
+                                </TableCell>
+                              </TableRow>
+                            </TableBody>
+                          </Table>
+                        </TableContainer>
+                      ))}
+                    {articles.filter(article => article.companies.some(comp => comp.id === company.companyId))
+                      .length === 0 && <Typography variant='body2'>No latest competition news</Typography>}
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          )}
           <Box display='flex' justifyContent='center' pb={4}>
             <Button color='primary' onClick={handleCloseContainer}>
               Close
